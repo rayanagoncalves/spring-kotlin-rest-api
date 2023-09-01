9a47b36f3ded3df9befe0c10eafaaa1794e979f5
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-09-01 17:05:30 -0300

    Criando o projeto

---
+++ src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/SpringKotlinRestApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.rayanagoncalves.spring.kotlin.rest.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinRestApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinRestApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/SpringKotlinRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.rayanagoncalves.spring.kotlin.rest.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinRestApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
