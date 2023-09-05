f8f51f6408a4e620e726c2337eafcc0422c4bdfd
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-09-04 21:22:31 -0300

    Criando endpoint para cadastrar tópicos

--- src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/controller/TopicController.kt
+++ src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/controller/TopicController.kt
@@ -2,10 +2,7 @@ package br.com.rayanagoncalves.spring.kotlin.rest.api.controller
 
 import br.com.rayanagoncalves.spring.kotlin.rest.api.model.Topic
 import br.com.rayanagoncalves.spring.kotlin.rest.api.service.TopicService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/topics")
@@ -19,6 +16,11 @@ class TopicController(private val topicService: TopicService) {
     @GetMapping("/{id}")
     fun findById(@PathVariable id: Long): Topic {
         return topicService.findById(id)
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topic: Topic): Void {
+        topicService.cadastrar(topic)
     }
 
 }
--- src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/service/TopicService.kt
+++ src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/service/TopicService.kt
@@ -1,65 +1,10 @@
 package br.com.rayanagoncalves.spring.kotlin.rest.api.service
 
-import br.com.rayanagoncalves.spring.kotlin.rest.api.model.Course
 import br.com.rayanagoncalves.spring.kotlin.rest.api.model.Topic
-import br.com.rayanagoncalves.spring.kotlin.rest.api.model.User
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
-class TopicService(private var topics: List<Topic>) {
-
-    init {
-        val topic = Topic(
-            id = 1,
-            title = "Dúvida Kotlin",
-            message = "Variáveis no Kotlin",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        val topic2 = Topic(
-            id = 2,
-            title = "Dúvida Kotlin 2",
-            message = "Variáveis no Kotlin 2",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        val topic3 = Topic(
-            id = 3,
-            title = "Dúvida Kotlin 3",
-            message = "Variáveis no Kotlin 3",
-            course = Course(
-                id = 1,
-                name = "Kotlin",
-                category = "Programacao"
-            ),
-            author = User(
-                id = 1,
-                name = "Ana da Silva",
-                email = "ana@email.com"
-            )
-        )
-
-        topics = Arrays.asList(topic, topic2, topic3)
-    }
+class TopicService(private var topics: List<Topic> = ArrayList()) {
 
     fun list(): List<Topic> {
         return this.topics
@@ -68,5 +13,9 @@ class TopicService(private var topics: List<Topic>) {
     fun findById(id: Long): Topic {
         return this.topics.stream().filter{ topic -> topic.id == id }
             .findFirst().get()
+    }
+
+    fun cadastrar(topic: Topic) {
+        this.topics.plus(topic)
     }
 }
