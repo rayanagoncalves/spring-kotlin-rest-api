e612fd00a1731c8061325dda0d81f061562620bb
Rayana Gonçalves <rayana.goncalves.cunha@hotmail.com>
2023-09-20 21:02:40 -0300

    Adicionando dependência do cache do Spring

--- src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/SpringKotlinRestApiApplication.kt
+++ src/main/kotlin/br/com/rayanagoncalves/spring/kotlin/rest/api/SpringKotlinRestApiApplication.kt
@@ -2,8 +2,10 @@ package br.com.rayanagoncalves.spring.kotlin.rest.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class SpringKotlinRestApiApplication
 
 fun main(args: Array<String>) {
